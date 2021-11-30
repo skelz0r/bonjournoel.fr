3e68296f4cd92a3b9da5d45fdc577ea9532d6b39
Loïc Delmaire <loic.delmaire@gmail.com>
2021-11-30 17:31:24 +0100

    Harden GeneratePost

--- lib/generate_post.rb
+++ lib/generate_post.rb
@@ -25,7 +25,7 @@ class GeneratePost
     variables = {
       'number' => date.day,
       'countdown' => christmas_day.yday - date.yday,
-      'date' => (date.to_time + 8.hours).to_s,
+      'date' => (date.to_time + 8*60).to_s,
 
       'christmas_tree_image_url' => christmas_tree_image_url,
       'spotify_track_id' => spotify_track_id,
